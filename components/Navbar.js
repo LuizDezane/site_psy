6bc55f9c5bae21176c842978af0c163e638c378e
Piotr <piotr.bania@europe.com>
2022-11-07 15:14:01 +0000

    init

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,10 +7,23 @@ const Navbar = () => {
     <section id='navbar'>
         <div className="container">
             <header>
-                <Link href="/"><Image className='logo' src="/logo.svg" alt="logo" width={184.34} height={46.77}/></Link>
+                <div className="upper_header">
+                    <div>
+                        <Image className='icon' src="/phone.svg" alt="logo" width={35} height={35}/>
+                        <p>0212 123 45 67</p>
+                    </div>
+                    <div>
+                        <Link href="/"><Image className='logo' src="/vercel.svg" alt="logo" width={200} height={35}/></Link>
+                    </div>
+                    <div>
+                        <Image className='icon' src="/instagram.svg" alt="logo" width={35} height={35}/>
+                        <Image className='icon' src="/facebook.svg" alt="logo" width={35} height={35}/>
+                    </div>
+                </div>
+
                 <nav>
                     <li>
-                        <Link href="/">Stroma główna </Link>
+                        <Link href="/">Strona główna </Link>
                         <Link href="/o_mnie">O mnie</Link>
                         <Link href="komu_pomagam">Komu pomagam</Link>
                         <Link href="jak_pomagam">Jak pomagam</Link>
@@ -18,7 +31,8 @@ const Navbar = () => {
                         <Link href="/blog">Blog</Link>
                         <Link href="/kontakt">Kontakt</Link>
                     </li>
-                    <Link href="/umow_wizyte"><button>Umów wizytę</button></Link>
+                    <Link href="/umow_wizyte"><button className='orange'>Umów wizytę</button></Link>
+                    <Link href="/umow_wizyte"><button className='white'>Umów wizytę</button></Link>
                 </nav>
             </header>
         </div>
