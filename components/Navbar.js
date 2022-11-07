7fbaccf1b148a76f6324a0b0b537d4fc236d7cbb
Piotr <piotr.bania@europe.com>
2022-11-07 23:19:39 +0000

    init

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,41 +3,48 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 const Navbar = () => {
-  return (
-    <section id='navbar'>
-        <div className="container">
-            <header>
-                <div className="upper_header">
-                    <div>
-                        <Image className='icon' src="/phone.svg" alt="logo" width={35} height={35}/>
-                        <p>0212 123 45 67</p>
-                    </div>
-                    <div>
-                        <Link href="/"><Image className='logo' src="/vercel.svg" alt="logo" width={200} height={35}/></Link>
-                    </div>
-                    <div>
-                        <Image className='icon' src="/instagram.svg" alt="logo" width={35} height={35}/>
-                        <Image className='icon' src="/facebook.svg" alt="logo" width={35} height={35}/>
-                    </div>
+return (
+<section id='navbar'>
+    <div className="container">
+        <header>
+
+            <div className="upper_header">
+                <div className='div1'>
+                    <Image className='icon' src="/phone.svg" alt="logo" width={35} height={35} />
+                    <p>0212 123 45 67</p>
                 </div>
 
-                <nav>
-                    <li>
-                        <Link href="/">Strona główna </Link>
-                        <Link href="/o_mnie">O mnie</Link>
-                        <Link href="komu_pomagam">Komu pomagam</Link>
-                        <Link href="jak_pomagam">Jak pomagam</Link>
-                        <Link href="/cennik">Cennik</Link>
-                        <Link href="/blog">Blog</Link>
-                        <Link href="/kontakt">Kontakt</Link>
-                    </li>
-                    <Link href="/umow_wizyte"><button className='orange'>Umów wizytę</button></Link>
-                    <Link href="/umow_wizyte"><button className='white'>Umów wizytę</button></Link>
-                </nav>
-            </header>
-        </div>
-    </section>
-  )
+                <div className='div2'>
+                    <Link href="/">
+                    <Image className='logo' src="/vercel.svg" alt="logo" width={120} height={60} />
+                    </Link>
+                </div>
+
+                <div className='div3'>
+                    <Image className='icon' src="/instagram.svg" alt="logo" width={35} height={35} />
+                    <Image className='icon' src="/facebook.svg" alt="logo" width={35} height={35} />
+                </div>
+            </div>
+
+            <div className="lower_header">
+                <li className='div4'>
+                    <Link href="/">Strona główna </Link>
+                    <Link href="/o_mnie">O mnie</Link>
+                    <Link href="komu_pomagam">Komu pomagam</Link>
+                    <Link href="jak_pomagam">Jak pomagam</Link>
+                    <Link href="/cennik">Cennik</Link>
+                    <Link href="/blog">Blog</Link>
+                    <Link href="/kontakt">Kontakt</Link>
+                </li>
+                <Link href="/umow_wizyte" className='div5'><button className='orange'>Umów wizytę</button></Link>
+                {/*
+                <Link href="/umow_wizyte"><button className='white'>Umów wizytę</button></Link> */}
+            </div>
+
+        </header>
+    </div>
+</section>
+)
 }
 
 export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,39 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Paulina Janik | Psycholog, Psychoterapeuta</title>
-        <meta name="description" content="Paulina Janik | Psycholog, Psychoterapeuta" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+return (
+<div>
 
-      <main>
+  <Head>
+    <title>Paulina Janik | Psycholog, Psychoterapeuta</title>
+    <meta name="description" content="Paulina Janik | Psycholog, Psychoterapeuta" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+
+  <main>
+    <section>
+      <div className="container">
         <h1>Header.</h1>
         <p>Paragraph.</p>
-      </main>
 
-      <footer>
+        <h1>Header.</h1>
+        <p>Paragraph.</p>
+        <h1>Header.</h1>
+        <p>Paragraph.</p>
+        <h1>Header.</h1>
+        <p>Paragraph.</p>
+        <h1>Header.</h1>
+        <p>Paragraph.</p>
+        <h1>Header.</h1>
+        <p>Paragraph.</p>
+        
+      </div>
+    </section>
+  </main>
 
-      </footer>
-    </div>
-  )
+  <footer>
+
+  </footer>
+</div>
+)
 }
