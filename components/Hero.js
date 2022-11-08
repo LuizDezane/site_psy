08286e78da289a98f04743b64e8d57cf8f0b1663
Piotr <piotr.bania@europe.com>
2022-11-08 22:48:03 +0000

    init

--- components/Hero.js
+++ components/Hero.js
@@ -1,31 +1,40 @@
 import React from 'react'
+import Link from 'next/link'
 
 import background_image from '../public/background.jpg'
 
 const Hero = () => {
 return (
-<div>
-    <div style={{backgroundImage: `url(${background_image.src})`,
+<div className='hero'>
+    {/* <div className='background' style={{backgroundImage: `url(${background_image.src})`,
         width: '100%',
-        height: '80vh',
-        opacity: 0.75}}>
-    </div>
-    <div>
-        <h1>Gdy słucham co mówisz,
-            słyszę kim jesteś</h1>
-        <h4>Ralph Waldo Emerson</h4>
-    </div>
-    <div>
-        <h4>Paulina Janik<br />
-            Psycholog, psychoterapeuta</h4>
-        <p>W swojej pacy kieruje się przede wszystkim dobrem drugiej osoby.
-            Zależy mi na tym, byś czuł się bezpiecznie
-            i swobodnie, dzieląc się ze mną swoimi historiami i doświadczeniami.
-            Gwarantuję zachowanie poufności.</p>
-    </div>
+        height: '82.22vh',
+        opacity: 0.75}}> */}
 
-</div>
-)
-}
+        <div className='background'>
+            <div className='slogan_top'>
+                <div className="area">
+                    <h1>Gdy słucham co mówisz,<br />
+                        słyszę kim jesteś</h1>
+                    <h4>Ralph Waldo Emerson</h4>
+                </div>
+            </div>
 
-export default Hero
+            <div className='slogan_bottom'>
+                <div className="area">
+                    <div className="image"></div>
+                    <h4>Paulina Janik<br />
+                        Psycholog, psychoterapeuta</h4>
+                    <p>W swojej pacy kieruje się przede wszystkim dobrem drugiej osoby.
+                        Zależy mi na tym, byś czuł się bezpiecznie
+                        i swobodnie, dzieląc się ze mną swoimi historiami i doświadczeniami.
+                        Gwarantuję zachowanie poufności.</p>
+                        <Link href="/umow_wizyte" className='button'><button className='orange'>Więcej o mnie...</button></Link>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+    }
+
+    export default Hero
