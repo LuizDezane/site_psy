29f71267f5c5a74f405d6b1a5f2478ee04813fc5
Piotr <piotr.bania@europe.com>
2022-11-10 21:19:45 +0000

    init

--- components/Hero.js
+++ components/Hero.js
@@ -1,35 +1,34 @@
-import React from 'react'
 import Link from 'next/link'
 
-import background_image from '../public/background.jpg'
-
 const Hero = () => {
-return (
-<div className='hero'>
-        <div className='background'>
-            <div className='slogan_top'>
-                <div className="area">
-                    <h1>Gdy słucham co mówisz,<br />
-                        słyszę kim jesteś</h1>
-                    <h4>Ralph Waldo Emerson</h4>
+    return (
+        <div className='section_A'>
+            <div className='background'>
+                <div className='slogan_top'>
+                    <div className="area">
+                        <h1>Gdy słucham co mówisz,<br />
+                            słyszę kim jesteś</h1>
+                        <h4>Ralph Waldo Emerson</h4>
+                    </div>
                 </div>
-            </div>
 
-            <div className='slogan_bottom'>
-                <div className="area">
-                    <div className="image"></div>
-                    <h4>Paulina Janik<br />
-                        Psycholog, psychoterapeuta</h4>
-                    <p>W swojej pacy kieruje się przede wszystkim dobrem drugiej osoby.
-                        Zależy mi na tym, byś czuł się bezpiecznie
-                        i swobodnie, dzieląc się ze mną swoimi historiami i doświadczeniami.
-                        Gwarantuję zachowanie poufności.</p>
-                        <Link href="/umow_wizyte" className='button'><button className='orange'>Więcej o mnie...</button></Link>
+                <div className='slogan_bottom'>
+                    <div className="area">
+                        <div className="image"></div>
+                        <div className="text">
+                            <h2>Paulina Janik<br />
+                            Psycholog, psychoterapeuta</h2>
+                            <p>W swojej pacy kieruje się przede wszystkim dobrem drugiej osoby.
+                            Zależy mi na tym, byś czuł się bezpiecznie
+                            i swobodnie, dzieląc się ze mną swoimi historiami i doświadczeniami.
+                            Gwarantuję zachowanie poufności.</p>
+                            <Link href="/umow_wizyte" className='button'><button className='orange'>Więcej o mnie...</button></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
-    }
+}
 
-    export default Hero
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 
 import Hero from '../components/Hero'
+import KomuPomagam from '../components/KomuPomagam'
 
 export default function Home() {
 return (
@@ -18,20 +18,53 @@ return (
     <section>
       <div className="container">
         <Hero />
-        <br /><br />
-
-        <h2>Konsultacje psychologiczne</h2>
-        <p>Proces psychoterapeutyczny poprzedzony jest konsultacjami psychologicznymi. Zazwyczaj cztery spotkania mają charakter konsultacji diagnostycznych. Jest to czas na dokładne zrozumienie problemu, a także osoby, która go doświadcza. Na tej podstawie ustalimy dalszą współpracę.</p>
-        <Link href="/umow_wizyte" className='button'><button className='white'>Dowiedz się więcej...</button></Link>
-        <br /><br />
-
-        <h2>Psychoterapia Indywidualna osób dorosłych</h2>
-        <p>Psychoterapia to przede wszystkim spotkanie dwóch osób, które polega na swobodnej  rozmowie toczącej się między pacjentem a terapeutą.</p>
-        <Link href="/umow_wizyte" className='button'><button className='white'>Dowiedz się więcej...</button></Link>
-        <br /><br />
-
-        </div>
+        <KomuPomagam />
+      </div>
     </section>
+
+    <h2>Pomagam</h2>
+    <div className="kolumna_1">
+      <h4>Radzić sobie z trudnościami natury emocjonalnej</h4>
+      <p>Zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja.</p>
+    </div>
+    <div className="kolumna_2">
+      <h4>Radzić sobie z problemami natury egzystencjalnej</h4>
+      <p>Samotność, brak poczucia sensu i celu życia, myśli samobójcze.</p>
+    </div>
+    <div className="kolumna_3">
+      <h4>Poprawić i/lub budować relacje z innymi</h4>
+      <p>Trudności w nawiązywaniu i utrzymywaniu relacji przyjacielskich lub partnerskich.</p>
+    </div>
+    <div className="kolumna_4">
+      <h4>Zrozumieć i zaakceptować siebie</h4>
+      <p>zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja.</p>
+    </div>
+    <div className="kolumna_5">
+      <h4>Skuteczniej funkcjonować</h4>
+      <p>Niskie poczucie własnej wartości, krytyczne postrzeganie siebie, nieśmiałość.</p>
+    </div>
+    <div className="kolumna_6">
+      <h4>Pokonać kryzysy osobiste</h4>
+      <p>Wyzwania, zmiany, ważne decyzje w życiu prywatnym lub zawodowym.</p>
+    </div>
+    <Link href="/umow_wizyte" className='button'><button className='orange'>Dowiedź sie wiecej...</button></Link>
+
+    <br /><br />
+
+    <h2>Ostatnio na Blogu</h2>
+    <h4>Psychoterapia Indywidualna osób dorosłych</h4>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+    <Link href="/umow_wizyte" className='button'><button className='orange'>Czytaj dalej...</button></Link>
+
+    <h4>Psychoterapia Indywidualna osób dorosłych</h4>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+    <Link href="/umow_wizyte" className='button'><button className='orange'>Czytaj dalej...</button></Link>
+
+    <h4>Psychoterapia Indywidualna osób dorosłych</h4>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+    <Link href="/umow_wizyte" className='button'><button className='orange'>Czytaj dalej...</button></Link>
+
+    <h3>Skontaktuj sie ze mną</h3>
   </main>
 
   <footer>
