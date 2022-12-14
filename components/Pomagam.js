ebd6bb18d5409929d76260282a871ec7b247e3fb
Piotr <piotr.bania@europe.com>
2022-12-14 13:19:44 +0000

    init

--- components/Pomagam.js
+++ components/Pomagam.js
@@ -3,46 +3,50 @@ import Link from 'next/link'
 const Pomagam = () => {
     return (
         <section className="pomagam">
+
             <h1 className="header_pomagam">Pomagam</h1>
-            <div className="pomagam_background">
-                    <div className="pomagam_1">
-                        <h4>Radzić sobie z trudnościami natury emocjonalnej</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja</p>
-                    </div>
 
-                    <div className="pomagam_2">
-                        <h4>Radzić sobie z problemami natury egzystencjalnej</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Samotność, brak poczucia sensu i celu życia, myśli samobójcze</p>
-                    </div>
-
-                    <div className="pomagam_3">
-                        <h4>Poprawić i/lub budować relacje z innymi</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Trudności w nawiązywaniu i utrzymywaniu relacji przyjacielskich lub partnerskich</p>
-                    </div>
-
-                    <div className="pomagam_4">
-                        <h4>Zrozumieć i zaakceptować siebie</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Samoakceptacja, trudności związane z orientacją seksualną</p>
-                    </div>
-
-                    <div className="pomagam_5">
-                        <h4>Skuteczniej funkcjonować</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja</p>
-                    </div>
-
-                    <div className="pomagam_6">
-                        <h4>Pokonać kryzysy osobiste</h4>
-                        <div className="line_pomagam"></div>
-                        <p>Niskie poczucie własnej wartości, krytyczne postrzeganie siebie, nieśmiałość</p>
-                    </div>
+            <div className="pomagam_div pomagam_1">
+                <h4>Radzić sobie z trudnościami natury emocjonalnej</h4>
+                <div className="line_pomagam"></div>
+                <p>Zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
             </div>
 
-            <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            <div className="pomagam_div pomagam_2">
+                <h4>Radzić sobie z problemami natury egzystencjalnej</h4>
+                <div className="line_pomagam"></div>
+                <p>Samotność, brak poczucia sensu i celu życia, myśli samobójcze</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            </div>
+
+            <div className="pomagam_div pomagam_3">
+                <h4>Poprawić i/lub budować relacje z innymi</h4>
+                <div className="line_pomagam"></div>
+                <p>Trudności w nawiązywaniu i utrzymywaniu relacji przyjacielskich lub partnerskich</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            </div>
+
+            <div className="pomagam_div pomagam_4">
+                <h4>Zrozumieć i zaakceptować siebie</h4>
+                <div className="line_pomagam"></div>
+                <p>Samoakceptacja, trudności związane z orientacją seksualną</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            </div>
+
+            <div className="pomagam_div pomagam_5">
+                <h4>Skuteczniej funkcjonować</h4>
+                <div className="line_pomagam"></div>
+                <p>Zaburzenia nastroju, lęk, niepokój, wycofanie, stres, smutek, złość i agresja</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            </div>
+
+            <div className="pomagam_div pomagam_6">
+                <h4>Pokonać kryzysy osobiste</h4>
+                <div className="line_pomagam"></div>
+                <p>Niskie poczucie własnej wartości, krytyczne postrzeganie siebie, nieśmiałość</p>
+                <Link href="/jak_pomagam" className='pomagam_button'><button className='dark'>Dowiedź sie wiecej</button></Link>
+            </div>
         </section>
     )
 }
