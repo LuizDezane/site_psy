2d94fae71a206da6916ca6868c1e350a485bd784
Piotr <piotr.bania@europe.com>
2023-09-26 13:25:25 +0100

    init

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -38,6 +38,7 @@ const Footer = () => {
                         <Image onClick={dialNumber} className='icon' src="/images/phone.svg" alt="phone" width={35} height={35} />
                         <Link href="https://www.linkedin.com/in/paulina-janik-a78205284/" target="_blank"><Image className='icon' src="/images/linkedin.svg" alt="linkedin" width={35} height={35} /></Link>
                         <Link href="https://www.facebook.com/profile.php?id=100087274075683&sk=services" target="_blank"><Image className='icon' src="/images/facebook.svg" alt="facebook" width={35} height={35} /></Link>
+                        <Link href="https://twojpsycholog.pl/profil-psychologa/paulina-janik-3521" target="_blank"><Image className='icon' src="/images/twoj_psycholog.svg" alt="twoj_psycholog" width={35} height={35} /></Link>
                     </div>
 
                     <div className='logo_menu'>
